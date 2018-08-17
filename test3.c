7a528e16a5aef1bc938accf2518b3b551d7468be
su <su@Suzy.(none)>
2018-08-17 16:35:31 +0800

    test4

--- test3.c
+++ test3.c
@@ -119,3 +119,102 @@ int test3(){
 	puts(tmp);
 
 }
+
+#include <termios.h>
+
+static void set_noecho(int fd){
+	struct termios stermios;
+	if(tcgetattr(fd,&stermios) < 0)
+		err_sys("tcgetattr error");
+	stermios.c_iflag &= ~(ECHO|ECHOE|ECHOK|ECHONL);
+	stermios.c_oflag &= ~(ONLCR);
+
+	if(tcsetattr(fd,TCSCAN,&stermios) < 0)
+		err_sys("tcsetattr error");
+
+}
+
+#define BUFFSIZE 512
+static void sig_term(int);
+
+int test4(int argc,char *argv[]){
+
+	int fdm,c,ignoreeof,interactive,noecho,verbose;
+	pid_t pid;
+	char *driver;
+	char slave_name[20];
+	struct termios orig_termios;
+	struct winsize size;
+	
+	interactive = isatty(STDIN_FILENO);
+	ignoreeof = 0;
+	noecho = 0;
+	verbose = 0;
+	driver = NULL;
+	
+	opterr = 0;
+	while((c = getopt(argc,argv,OPTSTR)) != EOF){
+		switch(c){
+			case 'd':
+			driver = optarg;
+			break;
+		case 'e':
+			noecho = 1;
+			break;
+		case 'i':
+			ignoreeof = 1;
+			break;
+		case 'n':
+			interactive = 0;
+			break;
+		case 'v':
+			verbose = 1;
+			break;
+		case '?':
+			err_quit("unrecongnized option -%c",optopt);
+		}
+	}
+	if(optind >= argc)
+		err_quit("usage:pty[-d driver -einv]progrep [arg ...]");
+
+	if(interactive){
+		if(tcgetattr(STDIN_FILENO,&orig_termios) < 0){
+			err_sys("tcgetattr error");
+		}
+		if(ioctl(STDIN_FILENO,TIOCGWINSZ,(char *)&size) < 0)
+			err_sys("ioctl error");
+		pid = pty_fork(&fdm,slave_name,sizeof(slave_name),NULL,NULL);
+
+	}else{
+		pid = pty_fork(&fdm,slave_name,sizeof(slave_name),NULL,NULL);
+	}
+	
+	if(pid < 0)
+		err_sys("fork error");
+
+	else if(pid == 0){
+		if(noecho)
+			set_noecho(STDIN_FILENO);
+
+		if(execvp(argv[optind],&argv[optind]) < 0)
+			err_sys("can't execute %s",argv[optind]);
+	}
+	if(verbose){
+		fprintf(stderr,"slave_name = %s\n",slave_name);
+		if(drive != NULL){
+			fprintf(stderr,"drive = %s\n",driver);
+		}
+	}
+	if(interactive && driver == NULL){
+		if(tty_raw(STDIN_FILENO) < 0)
+			err_sys("tty_raw error");
+		if(atexit(tty_atexit) < 0)
+			err_sys("atexit error");
+	}
+	if(drive)
+		do_driver(driver);
+	loop(fdm,ignoreeof);
+	exit(0);
+}
+
+
